1c460a26aff881d49524a3b516ad28c4d23ca5f0
Prestonw15 <prestonwatson.15@gmail.com>
2022-02-11 04:02:03 -0700

    added my variables as well as my api key so It will remain const and completed first function

---
+++ assests/script.js
@@ -0,0 +1,17 @@
+var cityList =$("#city-list");
+var cities = [];
+var key = "60cfa7935642c56d4fd52870a2aad2c7";
+
+//Format for day
+function FormatDay(date){
+    var date = new Date();
+    console.log(date);
+    var month = date.getMonth()+1;
+    var day = date.getDate();
+    
+    var dayOutput = date.getFullYear() + '/' +
+        (month<10 ? '0' : '') + month + '/' +
+        (day<10 ? '0' : '') + day;
+    return dayOutput;
+}
+
